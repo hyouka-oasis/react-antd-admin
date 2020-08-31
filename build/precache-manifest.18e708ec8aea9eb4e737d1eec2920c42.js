83508f1bd80e7000099541f684d264086bfb3bf9
Hyouka <35371952+HyoukaM@users.noreply.github.com>
2020-08-31 09:03:01 +0800

    Add files via upload

---
+++ build/precache-manifest.18e708ec8aea9eb4e737d1eec2920c42.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c84393f5fa5bb6d4ba105e30ff408a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7211c674e8d0404ea54a",
+    "url": "./static/css/2.54a9057b.chunk.css"
+  },
+  {
+    "revision": "24422acc07f3cb6fb787",
+    "url": "./static/css/main.0ba997e8.chunk.css"
+  },
+  {
+    "revision": "7211c674e8d0404ea54a",
+    "url": "./static/js/2.f8c55ec0.chunk.js"
+  },
+  {
+    "revision": "3a1302f7959e75c4af7fce7ceec20fae",
+    "url": "./static/js/2.f8c55ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24422acc07f3cb6fb787",
+    "url": "./static/js/main.3aaa1844.chunk.js"
+  },
+  {
+    "revision": "5eaf57e453492cb820bd",
+    "url": "./static/js/runtime-main.9aa4dcad.js"
+  },
+  {
+    "revision": "71a0caf3c249ba795bda1293134f477a",
+    "url": "./static/media/Athena.71a0caf3.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e1501238349b5194ad07b326967c413.js"
+  "./precache-manifest.18e708ec8aea9eb4e737d1eec2920c42.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
